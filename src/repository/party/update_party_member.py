a2de5ad1453de0b3e6f95d3c9a8ec7cbf9e4cdf3
taykim01 <taykim01@gmail.com>
2024-07-26 01:54:13 +0900

    fix: request for update member

--- src/repository/party/update_party_member.py
+++ src/repository/party/update_party_member.py
@@ -1,15 +1,21 @@
 import json
 from fastapi import APIRouter, HTTPException
+from pydantic import BaseModel
 from src.api.v1.read import read
 from src.api.v1.update import update
 from src.repository.party.party import Party
 
 router = APIRouter()
 
+class JoinPartyRequest(BaseModel):
+    party_id: str
+    user_name: str
+    available_slots: str
+
 @router.post("/update-party-member")
-def update_party_member(party_id: str, user_name: str, available_slots):
+def update_party_member(request: JoinPartyRequest):
     try:
-        response = read("PARTY", party_id).model_dump_json()
+        response = read("PARTY", request.party_id).model_dump_json()
         response = json.loads(response)
         data = response["data"]
         party_data = Party(
@@ -27,14 +33,14 @@ def update_party_member(party_id: str, user_name: str, available_slots):
             for date in date_list:
                 if slot["date"] == date.date:
                     if slot["time"] == date.time:
-                        if user_name not in date.users:
-                            date.users.append(user_name)
-                        if user_name in date.unavailable:
-                            date.unavailable.remove(user_name)
+                        if request.user_name not in date.users:
+                            date.users.append(request.user_name)
+                        if request.user_name in date.unavailable:
+                            date.unavailable.remove(request.user_name)
                 user_available_time.append(date)
 
         update_payload = {"date_list": user_available_time}
-        response = update("PARTY", party_id, update_payload)
+        response = update("PARTY", request.model_dump_jsonparty_id, update_payload)
 
         return {"success": True, "message": "파티 멤버 업데이트에 성공했습니다."}
     except Exception as e:
